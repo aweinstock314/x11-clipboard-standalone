0b90d9d95c42bc1455001defe1109d06825a33bd
Avi Weinstock <aweinstock314@gmail.com>
2015-03-28 14:33:38 -0400

    Transcribed the rest of xcout from xclip.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-#![feature(libc)]
+#![feature(collections, core, libc)]
 
 extern crate libc;
 extern crate xlib;
 
-use std::borrow::ToOwned;
-use std::ffi::CString;
 use std::mem::{size_of, transmute};
 
 use libc::*;
@@ -14,6 +12,7 @@ pub struct ClipboardContext {
     display: *mut Display,
     window: Window,
     selection: Atom,
+    utf8string: Atom,
 }
 
 impl ClipboardContext {
@@ -34,26 +33,43 @@ impl ClipboardContext {
         if sel == 0 {
             return Err("XA_CLIPBOARD")
         }
+        let utf8 = unsafe { XmuInternAtom(dpy, _XA_UTF8_STRING) };
+        if utf8 == 0 {
+            return Err("XA_UTF8_STRING")
+        }
         Ok(ClipboardContext {
             display: dpy,
             window: win,
             selection: sel,
+            utf8string: utf8,
         })
     }
-    pub fn get_contents(&self) -> String {
+    pub fn get_contents(&self) -> Result<String, &str> {
         enum XCOutState {
             None,
             SentConvSel,
             BadTarget,
             Incr,
         };
+        fn mach_itemsize(format: c_int) -> usize {
+            match format {
+                8 => size_of::<c_char>(),
+                16 => size_of::<c_short>(),
+                32 => size_of::<c_long>(),
+                _ => panic!("unexpected format for mach_itemsize: {}", format),
+            }
+        }
         fn xcout(dpy: *mut Display, win: Window, evt: &mut Vec<u8>,
                 sel: Atom, target: Atom, type_: &mut Atom, dest: &mut Vec<u8>,
                 context: &mut XCOutState) {
-            let pty_cstr = CString::new("SERVO_CLIPBOARD_OUT").unwrap(); // TODO: lazy_static! (possibly?)
-            let pty_atom = unsafe { XInternAtom(dpy, transmute::<*const c_char, *mut c_char>(pty_cstr.as_ptr()), 0) };
-            let incr_cstr = CString::new("INCR").unwrap();
-            let incr_atom = unsafe { XInternAtom(dpy, transmute::<*const c_char, *mut c_char>(incr_cstr.as_ptr()), 0) };
+            let pty_atom = unsafe { XInternAtom(dpy, b"SERVO_CLIPBOARD_OUT\0".as_ptr() as *mut i8, 0) };
+            let incr_atom = unsafe { XInternAtom(dpy, b"INCR\0".as_ptr() as *mut i8, 0) };
+
+            let mut buffer: *mut c_uchar = std::ptr::null_mut();
+            let mut pty_format: c_int = 0;
+            let mut pty_size: c_ulong = 0;
+            let mut pty_items: c_ulong = 0;
+
             match *context {
                 XCOutState::None => {
                     unsafe { XConvertSelection(dpy, sel, target, pty_atom, win, 0); } // CurrentTime = 0 = special flag (TODO: rust-xlib)
@@ -69,11 +85,6 @@ impl ClipboardContext {
                         *context = XCOutState::BadTarget;
                         return;
                     }
-                    println!("{:?}\n{:?}\n", evt, event._type);
-                    let mut buffer: *mut c_uchar = std::ptr::null_mut();
-                    let mut pty_format: c_int = 0;
-                    let mut pty_size: c_ulong = 0;
-                    let mut pty_items: c_ulong = 0;
                     unsafe {
                         XGetWindowProperty(dpy, win, pty_atom, 0, 0, 0, 0,
                                             transmute::<&mut Atom, *mut Atom>(type_), 
@@ -81,9 +92,17 @@ impl ClipboardContext {
                                             transmute::<&mut c_ulong, *mut c_ulong>(&mut pty_items),
                                             transmute::<&mut c_ulong, *mut c_ulong>(&mut pty_size),
                                             transmute::<&mut *mut c_uchar, *mut *mut c_uchar>(&mut buffer));
+                        // This XFree is present in xclib.c, but rustc doesn't like it, and I'm not quite sure why.
+                        /*
+                        src/main.rs:85:31: 85:77 error: mismatched types:
+                         expected `*mut libc::types::common::c95::c_void`,
+                            found `*mut libc::types::common::c95::c_void`
+                        (expected enum `libc::types::common::c95::c_void`,
+                            found a different enum `libc::types::common::c95::c_void`) [E0308]
+                        src/main.rs:85                         XFree(transmute::<*mut c_uchar, *mut c_void>(buffer));
+                        */
                         //XFree(transmute::<*mut c_uchar, *mut c_void>(buffer));
                     }
-                    println!("{:?} {:?} {:?} {:?}", buffer, pty_format, pty_size, pty_items);
                     if *type_ == incr_atom {
                         unsafe {
                             XDeleteProperty(dpy, win, pty_atom);
@@ -100,44 +119,82 @@ impl ClipboardContext {
                                             transmute::<&mut c_ulong, *mut c_ulong>(&mut pty_size),
                                             transmute::<&mut *mut c_uchar, *mut *mut c_uchar>(&mut buffer));
                     }
-                    fn mach_itemsize(format: c_int) -> usize {
-                        match format {
-                            8 => size_of::<c_char>(),
-                            16 => size_of::<c_short>(),
-                            32 => size_of::<c_long>(),
-                            _ => 0, // TODO: should this panic! instead?
+                    let pty_machsize: c_ulong = pty_items * (mach_itemsize(pty_format) as c_ulong);
+                    dest.push_all(unsafe { std::slice::from_raw_parts_mut(buffer, (pty_machsize as usize) / size_of::<u8>()) });
+                    *context = XCOutState::None;
+                },
+                XCOutState::BadTarget => panic!("should be unreachable"),
+                XCOutState::Incr => {
+                    let event: &mut XPropertyEvent = unsafe { transmute(evt.as_mut_ptr()) };
+                    if event._type != PropertyNotify {
+                        return;
+                    }
+                    if event.state != 0 { // 0 == PropertyNewValue
+                        return;
+                    }
+                    unsafe {
+                        XGetWindowProperty(dpy, win, pty_atom, 0, 0, 0, 0,
+                                            transmute::<&mut Atom, *mut Atom>(type_), 
+                                            transmute::<&mut c_int, *mut c_int>(&mut pty_format),
+                                            transmute::<&mut c_ulong, *mut c_ulong>(&mut pty_items),
+                                            transmute::<&mut c_ulong, *mut c_ulong>(&mut pty_size),
+                                            transmute::<&mut *mut c_uchar, *mut *mut c_uchar>(&mut buffer));
+                    }
+                    //XFree(transmute::<*mut c_uchar, *mut c_void>(buffer));
+                    if pty_size == 0 {
+                        unsafe {
+                            XDeleteProperty(dpy, win, pty_atom);
+                            XFlush(dpy);
                         }
+                        *context = XCOutState::None;
+                        return;
+                    }
+                    unsafe {
+                        XGetWindowProperty(dpy, win, pty_atom, 0, pty_size as c_long, 0, 0,
+                                            transmute::<&mut Atom, *mut Atom>(type_), 
+                                            transmute::<&mut c_int, *mut c_int>(&mut pty_format),
+                                            transmute::<&mut c_ulong, *mut c_ulong>(&mut pty_items),
+                                            transmute::<&mut c_ulong, *mut c_ulong>(&mut pty_size),
+                                            transmute::<&mut *mut c_uchar, *mut *mut c_uchar>(&mut buffer));
                     }
                     let pty_machsize: c_ulong = pty_items * (mach_itemsize(pty_format) as c_ulong);
                     dest.push_all(unsafe { std::slice::from_raw_parts_mut(buffer, (pty_machsize as usize) / size_of::<u8>()) });
+                    *context = XCOutState::None;
                 },
-                XCOutState::BadTarget => (),
-                XCOutState::Incr => (),
             }
         }
         let mut sel_buf = vec![];
         let mut sel_type = 0;
         let mut state = XCOutState::None;
         let mut event: Vec<u8> = vec![0; get_size_for_XEvent()];
-        let mut target = XA_STRING; // TODO: XA_UTF8_STRING(dpy)
+        let mut target = self.utf8string;
         loop {
-            println!("1");
             if let XCOutState::None = state {} else {
-                println!("2");
                 unsafe { XNextEvent(self.display, event.as_mut_ptr() as *mut XEvent) };
             }
-            println!("3");
             xcout(self.display, self.window, &mut event, self.selection, target, &mut sel_type, &mut sel_buf, &mut state);
+            if let XCOutState::BadTarget = state {
+                if target == self.utf8string {
+                    state = XCOutState::None;
+                    target = XA_STRING;
+                    continue;
+                }
+                else {
+                    return Err("unable to negotiate format");
+                }
+            }
+            if let XCOutState::None = state {
+                break;
+            }
         }
-        String::from_utf8(sel_buf).unwrap()
+        Ok(String::from_utf8_lossy(sel_buf.as_slice()).into_owned())
     }
 }
 
 impl Drop for ClipboardContext {
     fn drop(&mut self) {
-        println!("display is {:?}", self.display);
         let retcode = unsafe { XCloseDisplay(self.display) };
-        if retcode == 0 {
+        if retcode != 0 {
             panic!("XCloseDisplay failed. (return code {})", retcode);
         }
     }
@@ -145,8 +202,5 @@ impl Drop for ClipboardContext {
 
 fn main() {
     let clipboard_ctx = ClipboardContext::new().unwrap();
-    println!("{}", clipboard_ctx.get_contents());
-    println!("{:?}", _XA_CLIPBOARD);
-    println!("{:?}", size_of::<ClipboardContext>());
-    println!("{:?}", get_size_for_XEvent());
+    println!("{}", clipboard_ctx.get_contents().unwrap());
 }
